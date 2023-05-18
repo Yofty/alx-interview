50841bb77df0417ff4400c3c115edc0e2ec94914
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-18 13:55:18 -0700

    space correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,14 +10,13 @@ def validUTF8(data):
         if skip > 0:
             skip -= 1
             continue
-
         if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
             return False
         elif data[i] <= 0x7f:
             skip = 0
         elif data[i] & 0b11111000 == 0b11110000:
             span = 4
-            if n -i >= span:
+            if n - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
                     data[i + 1: i + span],
