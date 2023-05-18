c93629922322971ce528db2d36813c5aa32c80ce
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-18 13:52:03 -0700

    typo correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,7 +19,7 @@ def validUTF8(data):
             span = 4
             if n -i >= span:
                 next_body = list(map(
-                    lamba x: x & 0b11000000 == 0b10000000,
+                    lambda x: x & 0b11000000 == 0b10000000,
                     data[i + 1: i + span],
                 ))
                 if not all(next_body):
