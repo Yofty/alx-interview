4cb75be582b4c808c33bc6149451e8571629ed2c
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-26 18:50:42 +0000

    unlock

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""return n number"""
+
+
+def canUnlockAll(boxes):
+    """determines if yopu can open all the lockboxes"""
+    unlocked = set()
+
+    for box_id, bod in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
