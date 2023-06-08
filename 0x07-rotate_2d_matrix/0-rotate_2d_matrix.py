f508d9fd7e0808f1217ce60041f9521c2a891380
root <106748943+Yofty@users.noreply.github.com>
2023-06-08 05:47:55 -0700

    correction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,10 +7,10 @@ def rotate_2d_matrix(matrix):
     left, right = 0, len(matrix) - 1
 
     while left < right:
-        for i in range(right -left):
+        for i in range(right - left):
             top, bottom = left, right
             topLeft = matrix[top][left + i]
-            matrix[top][left + i] = matrix[bottom -i][left]
+            matrix[top][left + i] = matrix[bottom - i][left]
             matrix[bottom - i][left] = matrix[bottom][right - i]
             matrix[bottom][right - i] = matrix[top + i][right]
             matrix[top + i][right] = topLeft
