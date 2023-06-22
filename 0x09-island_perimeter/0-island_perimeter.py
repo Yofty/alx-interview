2b28392887882c3733d5fee08f69de38269500fc
root <106748943+Yofty@users.noreply.github.com>
2023-06-22 11:42:10 -0700

    correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,7 @@ def island_perimeter(grid):
     grid_m = len(grid) - 1
     lst_m = len(grid[0]) - 1
 
-    for lst_idx, 1st in enumerate(grid):
+    for lst_idx, lst in enumerate(grid):
         for land_idx, land in enumerate(lst):
             if land == 1:
                 if land_idx == 0:
@@ -34,7 +34,7 @@ def island_perimeter(grid):
                     if grid[lst_idx + 1][land_idx] == 0:
                         counter += 1
 
-                elif lst_idx == frid_m:
+                elif lst_idx == grid_m:
                     if grid[lst_idx -1][land_idx] == 0:
                         counter += 1
 
